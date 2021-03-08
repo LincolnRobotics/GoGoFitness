178d7c00507d0c1d5f40818bb2074daa7596c083
LincolnRobotics <80289456+LincolnRobotics@users.noreply.github.com>
2021-03-08 16:25:02 -0500

    GGF initial commit
    Initial add of all files

---
+++ GoGoFirst/app/src/main/java/com/gamecodeschool/gogofirst/AboutAppScreen.java
@@ -0,0 +1,14 @@
+package com.gamecodeschool.gogofirst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutAppScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_app_screen);
+    }
+}
---
+++ GoGoFirst/app/src/main/java/com/gamecodeschool/gogofirst/GuestScreen.java
@@ -0,0 +1,14 @@
+package com.gamecodeschool.gogofirst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuestScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_screen);
+    }
+}
